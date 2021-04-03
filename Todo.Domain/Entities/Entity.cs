4ba40b0f64737c0f8f57d10616f29c88a47b42fb
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-04-03 13:31:52 -0300

    Definindo o Contrato dos Comandos

---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Todo.Domain.Entities
+{
+    public abstract class Entity: IEquatable<Entity>
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+        // Método para comparação de entidades
+        public bool Equals(Entity other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
