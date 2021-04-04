59003e1f93dd1b4322cee73b3bb623fb90406c15
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-04-04 14:23:44 -0300

    Contrato de Handler

---
+++ Todo.Domain/Handlers/Contracts/IHandler.cs
@@ -0,0 +1,13 @@
+﻿using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Handlers.Contracts
+{
+    // Esse Handler irá gerenciar um ou mais comandos
+    // Essa interface sempre pode chamar 'IHandler' de 'T', desde que esse 'T' seja um 'ICommand'
+    // Ou seja, qualquer comando 'T' que for manipulado pelo 'Handler' deve, obrigatoriamente, implementar o 'ICommand' (contrato)
+    public interface IHandler<T> where T : ICommand
+    {
+        // O comando que será executado pelo 'Handle' é sempre do tipo 'T', e o retorno sempre será um 'ICommandResult'
+        ICommandResult Handle(T command);
+    }
+}
