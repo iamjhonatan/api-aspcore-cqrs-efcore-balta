aeafb81569cd37a1294e18e4cd5aec4d9e05dffb
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-04-04 15:00:08 -0300

    Fail Fast Validations

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Flunt.Notifications;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Handlers
 {
-    public class TodoHandler
+    public class TodoHandler : 
+        Notifiable,
+        IHandler<CreateTodoCommand>,
+        IHandler<UpdateTodoCommand>
     {
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            // buscando uma validação rápida para evitar o trabalho de fazer todo o processo com o banco e na hora de salvar, dar erro
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada!", command.Notifications);
+        }
+
+        public ICommandResult Handle(UpdateTodoCommand command)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
