c26e3a42963f53e4a4acd369195b81fa341eb276
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-04-04 19:50:59 -0300

    Testando a entidade

---
+++ Todo.Domain.Tests/EntityTests/TodoItemTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.EntityTests
+{
+    [TestClass]
+    public class TodoItemTests
+    {
+        private readonly TodoItem _validTodo = new ("Titulo aqui", "jhonatanmedeiros", DateTime.Now);
+
+        [TestMethod]
+        public void Dado_um_novo_todo_o_mesmo_nao_pode_ser_concluido()
+        {
+            Assert.AreEqual(_validTodo.Done, false);
+        }
+    }
+}
