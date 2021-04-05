c5636ce2557291393ea43c10fd9f0e50ca03d65b
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-04-05 20:23:36 -0300

    Data Context

---
+++ Todo.Domain.Infra/Contexts/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Infra.Contexts
+{
+    // DataContext (ou Contexto de Dados): é a representação do banco de dados em memória
+    // Serve, de começo, para dizer quais tabelas referenciam as entidades e vice-versa
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions <DataContext> options)
+            : base(options)
+        {
+        }
+
+        // Definir qual estrutura de tabelas/objetos
+        // DbSet: conjunto de dados de TodoItem
+        // Pegue uma tabela do SQLServer, chamada 'Todos', e mapeie para um 'TodoItem'
+        public DbSet<TodoItem> Todos { get; set; }
+    }
+}
