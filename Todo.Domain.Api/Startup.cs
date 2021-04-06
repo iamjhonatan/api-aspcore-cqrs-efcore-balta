67fbd4f434ee2cbfe62eab6638e6130bcc2fce99
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-04-05 22:33:17 -0300

    Gerenciando as Dependências

--- Todo.Domain.Api/Startup.cs
+++ Todo.Domain.Api/Startup.cs
@@ -5,7 +5,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Todo.Domain.Handlers;
 using Todo.Domain.Infra.Contexts;
+using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Api
 {
@@ -30,7 +32,9 @@ namespace Todo.Domain.Api
 
             services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
             // services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
-            
+
+            services.AddTransient<ITodoRepository, TodoRepository>();
+            services.AddTransient<TodoHandler, TodoHandler>();
 
             services.AddSwaggerGen(c =>
             {
